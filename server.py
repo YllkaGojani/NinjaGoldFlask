081c4396e697992066a88826f02f45ba4606367d
pinkshrub <pinkshrub@gmail.com>
2016-09-21 09:45:16 -0700

    Code Review :)
    This looks like you've a solid understanding of the session and hidden variable concepts in this assignment. There is room for improvement. Session should be used as minimally as possible, only holding information that needs to persist through the whole user-session. Things like color or the amount of gold generated each request should be done elsewhere. Things like an activity message log and the gold earned would be things to keep in session. I would suggest looking into a different way to display the activity messages, if you need help figuring out ways to do this, DM me on slack. Hint: session['activity_log'] = [ ]

--- server.py
+++ server.py
@@ -10,10 +10,17 @@ def index():
 
 @app.route('/process_money',methods=['POST'])
 def findGold():
+	# I would advise against relying this heavily on session. Session should ideally only contain information that is
+	# relevant to a user or this particular session. In this case, keeping track of the gold is a great use of session.
+	# Additionally, you use 'extra' space/time computing every value every time as opposed to just computing what you need to.
 	session['numberFarm'] = random.randrange(10,21)
 	session['numberCave'] = random.randrange(5,11)
 	session['numberHouse'] = random.randrange(2,6)
 	session['numberCasino']	= random.randrange(-50,51) 
+	# The use of flashed messages is okay, but i would suggest maybe creating a more persistent structure to keep track of
+	# all the events/messages your user has done during each gold session. having a session['activity_messages'] to keep track
+	# of or push messages to and display in your front-end would be one alternative. You can also experiment with keeping a 
+	# variable on the backend to do that. Of course, with that comes a different approach to coloring your messages. 
 	if(request.form['building'] == 'farm'): 
 		flash('Earned ' + str(session['numberFarm']) +' from the farm!')
 		session['gold'] += session['numberFarm']
@@ -31,6 +38,8 @@ def findGold():
 			flash('Lost ' + str(session['numberCasino']) + ' from the casino! Ouch...')
 			session['gold'] += session['numberCasino']
 			session['color'] = 'red'
+		# this is a 'lot' of repeated code. what if you only changed the color based on gold < 0 as opposed to the whole method?
+		# maybe something along the lines of if gold > 0, message.color
 		elif(session['numberCasino'] >= 0):
 			flash('Earned ' + str(session['numberCasino']) + ' from the casino!')
 			session['gold'] += session['numberCasino']
